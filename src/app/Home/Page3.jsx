c09628efa40f6d5a61c6001061ab4a9591c1df38
jayasurya <suryasarisa99@gmail.com>
2024-04-10 22:16:35 +0530

    initial

--- src/app/Home/Page3.jsx
+++ src/app/Home/Page3.jsx
@@ -6,30 +6,23 @@ import img3c from "@public/home/img3c.jpg";
 export default function Page3() {
   return (
     <div className="page3 page">
-      <div className="part1">
-        <div className="row">
-          <div className="column">
-            <div className="img1">
-              <img src={img3a} alt="img3a" />
-            </div>
-            <div className="img2">
-              <img src={img3b} alt="img3b" />
-            </div>
-          </div>
-          <div className="img3">
-            <img src={img3c} alt="img3c" />
-          </div>
+      <div className="part1"></div>
+      <div className="cross-bar"></div>
+      <div className="part2"></div>
+      <div className="row">
+        <div className="column">
+          <img src={img3a} className="img1" alt="img3a" />
+          <img src={img3b} className="img2" alt="img3b" />
         </div>
+        <img className="img3" src={img3c} alt="img3c" />
       </div>
-      <div className="part2">
-        <div className="part2-container">
-          <div className="title">WHY ONE NOVEL ?</div>
-          <div className="desc">
-            Flexible Hours, Unlimited Earnings, One Novel&apos;s MLM Model Gives
-            you the Freedom to Succed
-          </div>
-          <button className="floating-btn">JOIN NOW</button>
+      <div className="part2-container">
+        <div className="title">WHY ONE NOVEL ?</div>
+        <div className="desc">
+          Flexible Hours, Unlimited Earnings, One Novel&apos;s MLM Model Gives
+          you the Freedom to Succed
         </div>
+        <button className="floating-btn">JOIN NOW</button>
       </div>
     </div>
   );
--- src/app/Home/TopBar.jsx
+++ src/app/Home/TopBar.jsx
@@ -1,18 +1,31 @@
 import { Link } from "react-router-dom";
 import logo from "@public/logo.png";
+import { FaRegUser, FaUser } from "react-icons/fa";
+import { IoWalletOutline, IoWallet } from "react-icons/io5";
 export default function TopBar() {
   return (
     <nav>
       <img src={logo} className="logo" alt="" />
       <ul className="menu">
         <li>
-          <Link to="/product">Profile</Link>
+          <Link to="/product">
+            <FaRegUser className="outline" size={22} />
+            <FaUser className="fill" size={22} />
+          </Link>
         </li>
-        <li>
-          <Link to="/page3">Refer</Link>
-        </li>
-        <li>
-          <Link to="/about">Wallet</Link>
+
+        <li className="wallet">
+          <Link to="/about">
+            <IoWalletOutline size={24} className="outline" />
+            <IoWallet size={24} className="fill" />
+            <div className="wallet-box">
+              <p className="balance">
+                Balance:
+                <span className="amount"> $5000</span>
+              </p>
+              <button className="withdrawl-btn">With Drawl</button>
+            </div>
+          </Link>
         </li>
         <li className="long-elm">
           <Link to="/about">About</Link>
