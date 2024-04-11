802a88b15b07ab78a9c0b62bd3dee8dad693aafc
jayasurya <suryasarisa99@gmail.com>
2024-04-11 21:30:28 +0530

    added statusbar theme color changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router";
 import Home from "@app/Home/Home";
 import Register from "@app/Register";
@@ -7,6 +7,19 @@ import ProfilePage from "@app/Profile/Profile";
 import WithdrawlPage from "@app/Withdrawl/Withdrawl";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      let metaThemeColor = document.querySelector("meta[name=theme-color]");
+      if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
+        metaThemeColor.setAttribute("content", "#000000"); // Change this to the color you want for dark theme
+      } else {
+        metaThemeColor.setAttribute("content", "#FFFFFF"); // Change this to the color you want for light theme
+      }
+    });
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
